e0d174d909d8040f5ad3e218f18939551aa3c104
Iago Guedelha Nunes <iagognunes@outlook.com>
2021-04-22 16:59:38 -0300

    Aula 1

--- devquiz/lib/home/home_page.dart
+++ devquiz/lib/home/home_page.dart
@@ -11,10 +11,8 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-          child: Scaffold(
-        appBar: AppBarWidget(),
-      ),
+    return Scaffold(
+      appBar: AppBarWidget(),
     );
   }
 }
